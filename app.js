5a348fe829e22cb80cae1267de327172ffe25bc7
saakshii123 <onlycodingdot@gmail.com>
2024-06-11 12:48:46 +0530

    Changed port settings for vercel
    port settings for vercel

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-
-app.listen(3000, () => {
-    console.log('Server Started');
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
+});
